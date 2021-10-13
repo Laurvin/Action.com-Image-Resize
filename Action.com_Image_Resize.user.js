c078859c17750caab0765b4d4967ff3332ee0fb2
Laurvin <github@dbbv.com>
2021-10-13 14:38:54 +0200

    Create  Action.com_Image_Resize.user.js

---
+++ Action.com_Image_Resize.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name Action.com Image Resize
+// @namespace Action.com Image Resize
+// @author Laurvin
+// @description When directly viewing an image on Action.com, ensures that the highest resolution image is loaded.
+// @version 1.0
+// @icon http://i.imgur.com/XYzKXzK.png
+// @downloadURL
+// @include https://www.action.com/globalassets/*
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @grant none
+// @run-at document-idle
+// ==/UserScript==
+
+function DoesItExist() {
+    var url = window.location.href;
+    url = (url.split('?')[0]);
+    if (url == window.location.href) return;
+    $.ajax({
+        url: url,
+        type: 'HEAD',
+        success: function(data, textStatus, jqXHR) {
+            console.log('success');
+            window.location.replace(url);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log('error');
+        }
+    });
+}
+
+DoesItExist();
