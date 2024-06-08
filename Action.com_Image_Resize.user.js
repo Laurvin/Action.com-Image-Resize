0033d2d532a9936687b7b4056b62a7c4d3b7716f
Laurvin <github@dbbv.com>
2024-06-08 21:02:45 +0200

    Update Action.com_Image_Resize.user.js

--- Action.com_Image_Resize.user.js
+++ Action.com_Image_Resize.user.js
@@ -3,10 +3,11 @@
 // @namespace Action.com Image Resize
 // @author Laurvin
 // @description When directly viewing an image on Action.com, ensures that the highest resolution image is loaded.
-// @version 1.11
+// @version 1.2
 // @icon http://i.imgur.com/XYzKXzK.png
 // @downloadURL https://github.com/Laurvin/Action.com-Image-Resize/raw/main/Action.com_Image_Resize.user.js
-// @match https://www.action.com/_next/image/*
+// @updateURL https://github.com/Laurvin/Action.com-Image-Resize/raw/main/Action.com_Image_Resize.user.js
+// @match https://action.com/hostedassets/CMSArticleImages/*
 // @require https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @grant none
 // @run-at document-idle
@@ -14,25 +15,9 @@
 
 function DoesItExist() {
     var url = window.location.href;
-    url = (url.split('?url=')[1]);
-    url = (url.split('&')[0]);
+    url = (url.split('?width')[0]);
     url = decodeURIComponent(url);
-    url = url.replace('https://', 'https://www.');
     window.location.replace(url);
-/*
-    if (url == window.location.href) return;
-    $.ajax({
-        url: url,
-        type: 'HEAD',
-        success: function(data, textStatus, jqXHR) {
-            console.log('success');
-            window.location.replace(url);
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log('error');
-        }
-    });
-     */
 }
 
 DoesItExist();
